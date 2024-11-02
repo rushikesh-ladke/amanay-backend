4aa1683ffaf06ab9832fe84629148b1db0bdc645
rushikesh-ladke <rushiladke18.rsl@gmail.com>
2024-11-02 22:44:53 +0530

    deployment

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+console.log('NODE_ENV:', process.env.NODE_ENV);
+  const config = new DocumentBuilder()
+    .setTitle('User Management API')
+    .setDescription('API for user management and authentication')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(3000);
+  }
+
+  await app.init();
+  return app;
+}
+export default bootstrap;
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
